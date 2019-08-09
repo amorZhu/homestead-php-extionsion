1b6483b15309d3b69d9e9ec89740ee301e9c3185
zhushaolong <zhushaolong@cumulon.com>
2019-08-09 14:34:48 +0800

    update swoole

--- src/script/swoole-install.sh
+++ src/script/swoole-install.sh
@@ -2,7 +2,7 @@
 export DEBIAN_FRONTEND=noninteractive
 
 PHP_EXTENSION_NAME="swoole"
-PHP_EXTENSION_VERSION="4.4.1"
+PHP_EXTENSION_VERSION="4.4.3"
 PHP_EXTENSION_FILE_TYPE=".tgz"
 
 SYSTEM="linux"
@@ -10,42 +10,16 @@ SYSTEM="linux"
 # PHP Version: PHP 7.1.0 or newer
 # PEAR Package: PEAR 1.4.0 or newer
 
-if [ ! -e swoole-4.4.1.tgz ]; then
-    wget https://pecl.php.net/get/swoole-4.4.1.tgz
-    tar -zxf swoole-4.4.1.tgz
+if [ ! -e swoole-4.4.3.tgz ]; then
+    wget https://pecl.php.net/get/swoole-4.4.3.tgz
+    tar -zxf swoole-4.4.3.tgz
 fi;
 
-cd swoole-4.4.1
-
-# sudo DEBIAN_FRONTEND=noninteractive apt-get -y install php5.6-dev
-# phpize5.6 
-# ./configure --with-php-config=/usr/bin/php-config5.6 > /dev/null
-# make clean > /dev/null
-# make >/dev/null 2>&1
-# sudo make install
-# sudo chmod 644 /usr/lib/php/20131226/swoole.so
-# sudo bash -c "echo 'extension=swoole.so' > /etc/php/5.6/mods-available/swoole.ini"
-# sudo rm -rf /etc/php/5.6/cli/conf.d/20-swoole.ini /etc/php/5.6/fpm/conf.d/20-swoole.ini
-# sudo ln -s /etc/php/5.6/mods-available/swoole.ini /etc/php/5.6/cli/conf.d/20-swoole.ini
-# sudo ln -s /etc/php/5.6/mods-available/swoole.ini /etc/php/5.6/fpm/conf.d/20-swoole.ini
-# sudo service php5.6-fpm restart
-
-# sudo DEBIAN_FRONTEND=noninteractive apt-get -y install php7.0-dev
-# phpize7.0
-# ./configure --with-php-config=/usr/bin/php-config7.0 > /dev/null
-# make clean > /dev/null
-# make >/dev/null 2>&1
-# sudo make install
-# sudo chmod 644 /usr/lib/php/20151012/swoole.so
-# sudo bash -c "echo 'extension=swoole.so' > /etc/php/7.0/mods-available/swoole.ini"
-# sudo rm -rf /etc/php/7.0/cli/conf.d/20-swoole.ini /etc/php/7.0/fpm/conf.d/20-swoole.ini
-# sudo ln -s /etc/php/7.0/mods-available/swoole.ini /etc/php/7.0/cli/conf.d/20-swoole.ini
-# sudo ln -s /etc/php/7.0/mods-available/swoole.ini /etc/php/7.0/fpm/conf.d/20-swoole.ini
-# sudo service php7.0-fpm restart
+cd swoole-4.4.3
 
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install php7.1-dev
 phpize7.1
-./configure --with-php-config=/usr/bin/php-config7.1 > /dev/null
+./configure --with-php-config=/usr/bin/php-config7.1 --enable-openssl > /dev/null
 make clean > /dev/null
 make >/dev/null 2>&1
 sudo make install
@@ -57,7 +31,7 @@ sudo ln -s /etc/php/7.1/mods-available/swoole.ini /etc/php/7.1/fpm/conf.d/20-swo
 sudo service php7.1-fpm restart
 
 phpize7.2
-./configure --with-php-config=/usr/bin/php-config7.2 > /dev/null
+./configure --with-php-config=/usr/bin/php-config7.2 --enable-openssl > /dev/null
 make clean > /dev/null
 make >/dev/null 2>&1
 sudo make install
@@ -69,7 +43,7 @@ sudo ln -s /etc/php/7.2/mods-available/swoole.ini /etc/php/7.2/fpm/conf.d/20-swo
 sudo service php7.2-fpm restart
 
 phpize7.3
-./configure --with-php-config=/usr/bin/php-config7.3 > /dev/null
+./configure --with-php-config=/usr/bin/php-config7.3 --enable-openssl > /dev/null
 make clean > /dev/null
 make >/dev/null 2>&1
 sudo make install
